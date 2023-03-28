b977b9991f8af9a674ac4f8ed13bb75a69357383
SIST <SIST@DESKTOP-Q1Q6QPI>
2023-03-28 16:24:35 +0900

    1111

---
+++ FirstProject/src/MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");	// sysout + ctrl + space > println
+		System.out.println("Hello Java");   // ctrl + F11 > 실행하기
+		System.out.println("김윤영");
+		
+		}		
+
+	}
+
---
+++ FirstProject/src/MainClass2.java
@@ -0,0 +1,9 @@
+
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("홍길동");
+
+	}
+
+}
